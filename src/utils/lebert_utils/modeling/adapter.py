a3577658951c72d3174e8bdd6e11f2e956ecd51b
krisfu <1735573894@qq.com>
2022-03-11 11:53:19 +0800

    Update adapter.py

--- src/utils/lebert_utils/modeling/adapter.py
+++ src/utils/lebert_utils/modeling/adapter.py
@@ -23,23 +23,28 @@ class LexiconAdapter(nn.Module):
         self.attn_W = nn.Parameter(torch.zeros(config.hidden_size, config.hidden_size))
         self.attn_W.data.normal_(mean=0.0, std=config.initializer_range)
 
-    def cul_alpha(self, layer_output, word_outputs, input_word_mask):
-
+    def WordAttention(self, layer_output, word_outputs, input_word_mask):
         alpha = torch.matmul(layer_output.unsqueeze(2), self.attn_W)
         alpha = torch.matmul(alpha, torch.transpose(word_outputs, 2, 3))
         alpha = alpha.squeeze()
         alpha = alpha + (1 - input_word_mask.float()) * (-10000.0)
         alpha = torch.nn.Softmax(dim=-1)(alpha)
         alpha = alpha.unsqueeze(-1)
-
         return alpha
 
-    def forward(self, layer_output, input_word_embeddings, input_word_mask):
+    def BiLinear_Attention(self, hidden_states, word_embedding, word_mask):
+        word_embedding = self.word_transform(word_embedding)
+        word_attention = self.WordAttention(hidden_states, word_embedding, word_mask)
+        word_embedding = torch.sum(word_embedding * word_attention, dim=2)
+        hidden_states = hidden_states + word_embedding
+        return hidden_states
 
-        word_outputs = self.word_transform(input_word_embeddings)
-        alpha = self.cul_alpha(layer_output, word_outputs, input_word_mask)
-        weighted_word_embedding = torch.sum(word_outputs * alpha, dim=2)
-        layer_output = layer_output + weighted_word_embedding
-        layer_output = self.fuse_layer(layer_output)
+    def Add_Norm(self, hidden_states):
+        hidden_states = self.fuse_layer(hidden_states)
+        return hidden_states
 
+    def forward(self, layer_output, word_embeddings, word_mask):
+        layer_output = self.BiLinear_Attention(layer_output, word_embeddings, word_mask)
+        layer_output = self.Add_Norm(layer_output)
         return layer_output
+
